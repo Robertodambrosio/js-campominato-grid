5cc7c9fd96e2dca41b1de9b356491968fbb8d676
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-17 20:03:32 +0100

    correzioni

--- js/script.js
+++ js/script.js
@@ -2,32 +2,31 @@
 let level = 0;
 
 do {
-level = parseInt(prompt("Scegli il tuo livello di difficoltà"));
-} while (level < 0 || level > 3){
-}
+    level = parseInt(prompt("Scegli il tuo livello di difficoltà"));
+} while (level < 0 || level > 3) {}
 console.log(level);
 
 const container = document.createElement('div');
 container.className = "container";
-document.body.appendChild(container); 
+document.body.appendChild(container);
 console.log(container);
 
-if (level == 1){
+if (level == 1) {
     container.classList.add("easy")
 
-    addSquare();
+    // addSquare();
     colorSquare(100);
 
-} else if (level ==  2){
+} else if (level == 2) {
     container.classList.add("medium")
 
-    addSquare()
+    // addSquare();
     colorSquare(81);
 
 } else if (level == 3) {
     container.classList.add("hard")
 
-    addSquare();
+    // addSquare();
     colorSquare(49);
 }
 
@@ -37,20 +36,22 @@ if (level == 1){
 
 
 
-function colorSquare (num) {
-for (let i = 1; i < num; i++) {
-let square = addSquare();
-square = document.getElementsByClassName("square");
-square[i].addEventListener('click', function() {
-    square[i].classList.add('correct');
-})
-}
+// function addSquare() {
+//     const square = document.createElement('div')
+//     square.className = 'square';
+//     container.appendChild(square);
+
+//     console.log(square);
+// }
+
+function colorSquare(num) {
+    for (let i = 0; i < num; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        container.appendChild(square);
+        square.addEventListener('click', function () {
+            square.classList.add('correct');
+        });
+    }
 }
 
-function addSquare () {
-    const square = document.createElement('div')
-    square.className = 'square';
-    container.appendChild(square);
-
-    console.log(square);
-}
