09cf8255e687c61aea453456dd738eea0f169349
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-17 15:46:19 +0100

    push ticket

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
-const container = document.querySelector(".container")
+// PROVA
+const level = parseInt(prompt("Scegli il tuo livello di difficoltà"));
+console.log(level);
 
-let items = '';
+const container = document.createElement('div');
+container.className = "container";
+document.body.appendChild(container); 
+console.log(container);
+
+if (level == 1){
+    container.classList.add("easy")
+
+    for(let i = 0; i < 100; i++) {
+        addSquare();
+    }
+
+} else if (level ==  2){
+    container.classList.add("medium")
+
+    for(let i = 0; i < 81; i++) {
+        addSquare()
+    }
+
+} else if (level == 3) {
+    container.classList.add("hard")
+
+    for(let i = 0; i < 49; i++) {
+        addSquare();
+    }
+}
+
+
+
+
+
+
+
+
+
+function addSquare () {
+    const square = document.createElement('div')
+    square.className = 'square';
+    container.appendChild(square);
+
+    console.log(square);
+}
